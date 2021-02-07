7a6142a2c896d78edcdf011fba795a0b821f7d11
Hairton Vanda <39865616+hairtonvanda18@users.noreply.github.com>
2021-02-07 21:45:19 +0100

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -1,7 +1,54 @@
-import pygame as P
+import pygame as p
 import ChessEngine
-
+    
+    
 WIDTH = HEIGHT = 400
 DIMENSION = 5 
 SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+    pieces = ["bR","bN","bB","bQ","bK",
+            "bp","wp","wR","wN","wB","wQ","wK"]
+    for piece in pieces:
+        IMAGES[piece] =p.transform.scale( p.image.load("Images/"+piece+".png"),(SQ_SIZE,SQ_SIZE))
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen,gs):
+    drawBoard(screen)
+    drawPieces(screen,gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color("gray"),p.Color("white")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen,color,p.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+def drawPieces(screen,board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece],p.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+
+main()
+
 
