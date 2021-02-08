af331bc8227e045cf766ac158f6d1e859a53f4ff
Hairton Vanda <39865616+hairtonvanda18@users.noreply.github.com>
2021-02-08 16:43:28 +0100

    Check, Checkmate and Stale mate working

--- ChessEngine.py
+++ ChessEngine.py
@@ -13,6 +13,8 @@ class GameState():
         self.moveLog=[]
         self.whiteKingLocation=(4,4)
         self.blackKingLocation=(0,4)
+        self.checkMate = False
+        self.staleMate = False
     def makeMove(self,move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
@@ -22,15 +24,50 @@ class GameState():
             self.whiteKingLocation = (move.endRow,move.endCol)
         elif move.pieceMoved == "bK":
             self.blackKingLocation =(move.endRow,move.endCol)
-            
+
     def undoMove(self):
         if len(self.moveLog) != 0:
             move = self.moveLog.pop()
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.whiteToMove = not self.whiteToMove
+            if move.pieceMoved == "wK":
+                self.whiteKingLocation = (move.startRow,move.startCol)
+            elif move.pieceMoved == "bK":
+                self.blackKingLocation =(move.startRow,move.startCol)
     def getValidMoves(self):
-        return self.getAllPossibleMoves()
+        moves = self.getAllPossibleMoves()
+        for i in range(len(moves)-1,-1,-1):
+            self.makeMove(moves[i])
+            self.whiteToMove = not self.whiteToMove
+            if self.inCheck():
+                moves.remove(moves[i])
+            self.whiteToMove = not self.whiteToMove
+            self.undoMove()
+        if len(moves) == 0:
+            if self.inCheck():
+                self.checkMate = True
+            else:
+                self.staleMate = True
+        else:
+            self.checkMate = False
+            self.staleMate = False
+        return moves 
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0],self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0],self.blackKingLocation[1])
+
+    def squareUnderAttack(self,r,c):
+        self.whiteToMove = not self.whiteToMove
+        oppMoves = self.getAllPossibleMoves() 
+        self.whiteToMove = not self.whiteToMove
+        for move in oppMoves:
+            if move.endRow == r and move.endCol == c:
+                return True
+        return False
+
     def getAllPossibleMoves(self):
         moves = []
         for r in range(len(self.board)):
